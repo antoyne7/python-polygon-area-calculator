13cf0d825cb4f02f675d49569731e051364ef857
Antoyne <antoine.brld@gmail.com>
2021-06-30 18:54:19 +0200

    pass all tests

--- shape_calculator.py
+++ shape_calculator.py
@@ -25,11 +25,21 @@ class Rectangle:
 
 
     def get_picture(self) -> str:
+        if self.width > 50 or self.height > 50:
+            return 'Too big for picture.'
+
         picture = '*'*self.width + '\n'
         for _ in range(self.height-1):
             picture += '*'*self.width + '\n'
 
         return picture
+
+    
+    def get_amount_inside(self, rectangle) -> int:
+        amount_width = self.width // rectangle.width
+        amount_height = self.height // rectangle.height
+        return amount_width * amount_height
+
 
     def __str__(self) -> str:
         return '{}(width={}, height={})'.format(self.__class__.__name__,
@@ -43,9 +53,19 @@ class Square(Rectangle):
         self.height = side
 
 
-    def set_side(self, side):
+    def set_side(self, side) -> None:
         self.set_height(side)
         self.set_width(side)
+
+    
+    def set_width(self, width) -> None:
+        self.width = width
+        self.height = width
+
+    
+    def set_height(self, height) -> None:
+        self.width = height
+        self.height = height
 
     
     def __str__(self) -> str:
